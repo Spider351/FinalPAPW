6a4e948f85f21e09d99e39b25db2e68ef54f696f
Javier Ornelas Valles <naitomea351@gmail.com>
2019-05-20 01:35:06 -0500

    Revert "Aquí hubo error"
    This reverts commit 57c9efb881f2ac84b4ab22eaa2d3328d5c6eb2b3.

--- src/java/dao/userDAO.java
+++ src/java/dao/userDAO.java
@@ -14,7 +14,7 @@ public class userDAO {
     public user login(String user, String pass) {
 
         try {
-            String sql = "select * from usuarios where nick=? and password =? limit 1";
+            String sql = "select * from usuarios where nick=? and password = md5(?) limit 1";
             PreparedStatement preparedStatement = conn.getConnection().prepareStatement(sql);
             preparedStatement.setString(1, user);
             preparedStatement.setString(2, pass);
@@ -23,7 +23,6 @@ public class userDAO {
             while (rs.next()) {
                 usuario.setId(rs.getInt("id"));
                 usuario.setNombre(rs.getString("nombre"));
-                usuario.setApellido(rs.getString("apellido"));
                 usuario.setEmail(rs.getString("email"));
                 usuario.setNick(rs.getString("username"));
                 usuario.setPassword(rs.getString("password"));
@@ -35,31 +34,4 @@ public class userDAO {
             return null;
         }
     }
-    
-    public boolean insert() {
-        /*try {
-            String sql = "insert into usuarios values (?,?,?,?,?,?,?)";
-            PreparedStatement preparedStatement = conn.getConnection().prepareStatement(sql);
-            preparedStatement.setInt(1, usuario.getId());
-            preparedStatement.setString(2, usuario.getNombre());
-            preparedStatement.setString(3, usuario.getApellido());
-            preparedStatement.setString(4, usuario.getNick());
-            preparedStatement.setString(5, usuario.getEmail());
-            preparedStatement.setString(6, usuario.getPassword());
-            preparedStatement.setInt(7, usuario.getTarjeta());
-            
-            preparedStatement.setInt(1, 3);
-            preparedStatement.setString(2, "Javier");
-            preparedStatement.setString(3, "Ornelas");
-            preparedStatement.setString(4, "Spider351");
-            preparedStatement.setString(5, "spider351@correo.com");
-            preparedStatement.setString(6, "password123");
-            preparedStatement.setLong(7, Long.parseUnsignedLong("1111222233334444"));
-            preparedStatement.executeUpdate();
-            return true;
-        } catch (SQLException e) {
-            System.out.println("Error VacanteDao.insert: " + e.getMessage());
-            return false;
-        }*/
-    return true;}
 }
